336319a22b3b893b2e111973c428debbd0c7a294
Shivichouhan <shivichouhan9926@gmail.com>
2025-12-18 19:56:41 +0530

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,24 +1,15 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import seaborn as sns\n",
-    "import matplotlib.pyplot as plt\n",
-    "df = sns.load_dataset('tips')\n",
-    "sns.barplot(x='day', y='total_bill', data=df)\n",
-    "plt.show()\n",
-    "sns.scatterplot(x='total_bill', y='tip', data=df)\n",
-    "plt.show()"
-   ]
-  }
- ],
- "metadata": {
-  "language_info": { "name": "python" }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Dataset load
+df = sns.load_dataset('tips')
+
+# Barplot
+sns.barplot(x='day', y='total_bill', data=df)
+plt.title('Total Bill by Day')
+plt.show()
+
+# Scatterplot
+sns.scatterplot(x='total_bill', y='tip', data=df)
+plt.title('Total Bill vs Tip')
+plt.show()
